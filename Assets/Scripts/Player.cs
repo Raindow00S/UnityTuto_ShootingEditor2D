a938993b638aaad0ecaefead7fe7dd72988528b9
xiaoxichen <xiaoxi.chen@centurygame.com>
2023-02-28 14:14:05 +0800

    34 角色跳跃和触地检测

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace ShootingEditor2D
@@ -8,18 +5,38 @@ namespace ShootingEditor2D
     public class Player : MonoBehaviour
     {
         private Rigidbody2D mRigidbody2D;
+        private Trigger2DCheck mGroundCheck;
         private float mSpeed = 7f;  // todo 设置读写权限
+
+        private bool mJumpPressed;
         
         private void Awake()
         {
             mRigidbody2D = GetComponent<Rigidbody2D>();
+            mGroundCheck = transform.Find("GroundCheck").GetComponent<Trigger2DCheck>();
         }
 
         private void FixedUpdate()  // tip 在update里移动可能会穿透
         {
+            // 水平移动
             var horizontalInput = Input.GetAxis("Horizontal");
-
             mRigidbody2D.velocity = new Vector2(horizontalInput * mSpeed, mRigidbody2D.velocity.y);
+
+            // 跳跃（防止空中连跳）
+            var grounded = mGroundCheck.Triggered;
+            if (mJumpPressed && grounded)
+            {
+                mRigidbody2D.velocity = new Vector2(mRigidbody2D.velocity.x, mSpeed);
+            }
+
+            mJumpPressed = false;
+        }
+
+        private void Update()   // tip 输入在update里处理
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+                mJumpPressed = true;
+
         }
     }   
 }
---
+++ Assets/Scripts/Trigger2DCheck.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+namespace ShootingEditor2D
+{
+    public class Trigger2DCheck : MonoBehaviour
+    {
+        public LayerMask TargetLayers;  // 一个mask是对多个layer的指示
+        public int EnterCount;
+
+        public bool Triggered
+        {
+            get { return EnterCount > 0; }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (IsInLayerMask(other.gameObject, TargetLayers))
+                EnterCount++;
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (IsInLayerMask(other.gameObject, TargetLayers))
+                EnterCount--;
+        }
+
+        bool IsInLayerMask(GameObject obj, LayerMask mask)
+        {
+            var objLayerMask = 1 << obj.layer;
+            return (mask & objLayerMask) > 0;
+        }
+    }
+}
