611a07b491c5cad6ff7778de2824316b164e3db5
xiaoxichen <xiaoxi.chen@centurygame.com>
2023-02-28 15:58:13 +0800

    40 摄像机跟随

---
+++ Assets/Scripts/ViewController/Gameplay/CameraController.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+namespace ShootingEditor2D
+{
+    public class CameraController : MonoBehaviour
+    {
+        private Transform mPlayerTrans;
+
+        private void Update()
+        {
+            if (!mPlayerTrans)
+            {
+                var playerGO = GameObject.FindWithTag("Player");
+
+                if (playerGO)
+                {
+                    mPlayerTrans = playerGO.transform;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            
+            var cameraPos = transform.position;
+            var playerPos = mPlayerTrans.position;
+            cameraPos.x = playerPos.x + 3;
+            cameraPos.y = playerPos.y + 2;
+
+            transform.position = cameraPos;
+        }
+        
+        
+    }
+}
